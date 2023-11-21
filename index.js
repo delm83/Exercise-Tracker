fd5c3be91780d02eb941b74ebb14df7176055336
delm83 <delm83@googlemail.com>
2023-11-21 07:06:28 +0000

    added post user function

--- index.js
+++ index.js
@@ -7,6 +7,13 @@ const mongoose = require('mongoose');
 
 mongoose.connect(process.env['MONGO_URI'], { useNewUrlParser: true, useUnifiedTopology: true});
 
+let Schema = mongoose.Schema;
+let userSchema = new Schema({
+  username: String
+});
+
+let User = mongoose.model("User", userSchema);
+
 app.use(cors())
 app.use(express.static('public'))
 app.get('/', (req, res) => {
@@ -14,9 +21,17 @@ app.get('/', (req, res) => {
 });
 app.use(bodyParser.urlencoded({ extended: false }));
 
-
-
-
+app.post('/api/users', async (req, res)=>{
+  let inputName = await User.findOne({username: req.body.username})
+  if (inputName) {
+    res.json({username: inputName.username, _id: inputName.id})} 
+else 
+{
+ inputName = new User({username: req.body.username})
+ await inputName.save()
+ res.json({username: inputName.username, _id: inputName.id})
+}
+});
 
 const listener = app.listen(process.env.PORT || 3000, () => {
   console.log('Your app is listening on port ' + listener.address().port)
