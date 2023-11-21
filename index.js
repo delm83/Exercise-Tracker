15a3216c5420c960ea55e4b05fbed817ad420a02
delm83 <delm83@googlemail.com>
2023-11-21 08:35:47 +0000

    added post exercises function

--- index.js
+++ index.js
@@ -14,6 +14,24 @@ let userSchema = new Schema({
 
 let User = mongoose.model("User", userSchema);
 
+let exerciseSchema = new Schema({
+  userId: {
+  type: String,
+  required: true
+ },
+  description: {
+  type: String,
+  required: true
+ },
+  duration: {
+  type: Number,
+  required: true
+  },
+  date: Date
+  });
+
+let Exercise = mongoose.model('Exercise', exerciseSchema);
+
 app.use(cors())
 app.use(express.static('public'))
 app.get('/', (req, res) => {
@@ -38,6 +56,33 @@ app.get('/api/users', (req, res) =>{
     return res.json(users);
    });
 });
+
+app.post('/api/users/:_id/exercises', async (req, res)=>{
+  try{
+    let userId = req.params._id;
+    let description = req.body.description;
+    let duration = parseInt(req.body.duration);
+    let date = req.body.date;
+
+let inputUser = await User.findById(userId)
+        if (inputUser) {
+          !date ? date = new Date()
+          : date = new Date(date)
+          if(date.toString() === 'Invalid Date') {return res.json({error: date.toString()})}
+          
+            let inputExercise = new Exercise({
+                userId: userId,
+                description: description,
+                duration: duration,
+                date: date
+            });
+               await inputExercise.save();
+
+  res.json({_id: inputUser.id, username: inputUser.username, date: inputExercise.date.toDateString(), duration: duration, description: description})
+                }
+       else {throw "user not found"}
+      }catch(err){return res.json(err)}
+      });
 
 const listener = app.listen(process.env.PORT || 3000, () => {
   console.log('Your app is listening on port ' + listener.address().port)
