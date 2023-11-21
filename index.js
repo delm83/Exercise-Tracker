a22346d7aafb052c2c74d38696d80d20080344cf
delm83 <delm83@googlemail.com>
2023-11-21 06:47:38 +0000

    connect to mongodb

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
 const cors = require('cors')
-require('dotenv').config()
+const bodyParser = require("body-parser");
+const mongoose = require('mongoose'); 
+
+mongoose.connect(process.env['MONGO_URI'], { useNewUrlParser: true, useUnifiedTopology: true});
 
 app.use(cors())
 app.use(express.static('public'))
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
+app.use(bodyParser.urlencoded({ extended: false }));
 
 
 
